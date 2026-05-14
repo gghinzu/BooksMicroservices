0386c5c862c43fa16dcdd9eb4aeb98013611eb11
Goksu Uzun <120195166+gghinzu@users.noreply.github.com>
2026-05-14 20:46:43 +0300

    edit microservice architecture (apply clean arch.) - relocate files and add user DB

--- Books.APP/Domain/BooksDb.cs
+++ Books.APP/Domain/BooksDb.cs
@@ -8,10 +8,6 @@ namespace Books.APP.Domain
         public DbSet<Book> Books { get; set; }
         public DbSet<BookGenre> BookGenres { get; set; }
         public DbSet<Genre> Genres { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Role> Roles { get; set; }
-        public DbSet<UserRole> UserRoles { get; set; }
-        public DbSet<RefreshToken> RefreshTokens { get; set; }
 
         public BooksDb(DbContextOptions options) : base(options)
         {
