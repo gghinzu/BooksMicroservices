78d231d960b00bd595cfba27a10dfd88d32d520a
Goksu Uzun <120195166+gghinzu@users.noreply.github.com>
2026-05-14 21:33:41 +0300

    add tokencontroller

---
+++ Users.API/Controllers/TokenController.cs
@@ -0,0 +1,49 @@
+using CORE.APP.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Users.APP.Features.Tokens;
+
+namespace Users.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokensController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IConfiguration _configuration;
+
+        public TokensController(IMediator mediator, IConfiguration configuration)
+        {
+            _mediator = mediator;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(TokenRequest request)
+        {
+            request.SecurityKey = _configuration["SecurityKey"];
+            request.Issuer = _configuration["Issuer"];
+            request.Audience = _configuration["Audience"];
+
+            var response = await _mediator.Send(request);
+            if (response is not null)
+                return Ok(response);
+
+            return BadRequest(new CommandResponse(false, _configuration["TokenMessage:InvalidCredentials"]));
+        }
+
+        [HttpPost("Refresh")]
+        public async Task<IActionResult> Refresh(RefreshTokenRequest request)
+        {
+            request.SecurityKey = _configuration["SecurityKey"];
+            request.Issuer = _configuration["Issuer"];
+            request.Audience = _configuration["Audience"];
+
+            var response = await _mediator.Send(request);
+            if (response is not null)
+                return Ok(response);
+
+            return BadRequest(new CommandResponse(false, _configuration["TokenMessage:InvalidRefreshToken"]));
+        }
+    }
+}
--- Users.API/Program.cs
+++ Users.API/Program.cs
@@ -1,4 +1,9 @@
+using CORE.APP.Services.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 using Users.APP.Domain;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,14 +11,55 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString(nameof(UsersDb));
 builder.Services.AddDbContext<DbContext, UsersDb>(options => options.UseSqlite(connectionString));
 
+builder.Services.AddSingleton<ITokenAuthService, TokenAuthService>();
+
 foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
 {
     builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblies(assembly));
 }
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["SecurityKey"]!)),
+        ValidIssuer = builder.Configuration["Issuer"]!,
+        ValidAudience = builder.Configuration["Audience"]!
+    };
+});
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = JwtBearerDefaults.AuthenticationScheme,
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Bearer token\""
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = JwtBearerDefaults.AuthenticationScheme
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -25,6 +71,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
